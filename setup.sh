1a47cabfac3506bf369d1ae404da9a0c0a70a329
Fahmi Idris <fahmi@kata.ai>
2020-05-02 22:34:24 +0700

    chore: change docker compose and dockerfile

--- setup.sh
+++ setup.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 INIT_PATH=$(pwd)
 
-# Up gateway
-cd "$INIT_PATH/einfahrt";
-docker-compose up -d --build
+# Up Gateway
+cd "$INIT_PATH/einfart";
+docker-compose up -d
+
+# Up Booking
+cd "$INIT_PATH/auftrag";
+docker-compose up -d
+
+# UP Master
+cd "$INIT_PATH/meister";
+docker-compose up -d
 
 # UP Users
 cd "$INIT_PATH/verbraucher";
-docker-compose up -d --build
+docker-compose up -d
